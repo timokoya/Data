62400f87c78966ad8dbe0ab6a13e0e749b931d1d
Timi Okoya <125748895+timokoya@users.noreply.github.com>
2023-04-17 16:37:49 +0100

    Update DataPlay.py
    - extend to 25 rows (removed head)
    
    - replaced comma in column
    
    - replaced nan values with zero
    
    - bar chart extended to 16:9

--- DefensePoint/DataPlay.py
+++ DefensePoint/DataPlay.py
@@ -2,17 +2,23 @@ import csv
 import pandas as pd
 import matplotlib.pyplot as mp
 
-# read csv and take five rows
-data = pd.read_csv("data.csv", nrows=5)
- 
-# form dataframe
-data = data.head()
+# read csv and take set rows
+data = pd.read_csv("data.csv", nrows=25)
 
 # select 2 columns
 df = pd.DataFrame(data, columns=["discounted_price", "rating"])
 
 # remove the currency symbol in the discounted_price column
 df['discounted_price'] = df['discounted_price'].str.replace('₹', '')
+
+# remove the comma in the discounted_price column
+df['discounted_price'] = df['discounted_price'].str.replace(',', '')
+
+# replace nan values with zero
+df['discounted_price'] = df['discounted_price'].fillna(0)
+
+# replace nan values with zero
+df['rating'] = df['rating'].fillna(0)
 
 # cast the value in the discounted_price column to float
 df['discounted_price']=df['discounted_price'].astype(float)
@@ -21,7 +27,7 @@ df['discounted_price']=df['discounted_price'].astype(float)
 df['rating']=df['rating'].astype(float)
 
 # plot the dataframe
-df.plot(x="discounted_price", y=["rating"], kind="bar", figsize=(9, 8))
- 
+df.plot(x="discounted_price", y=["rating"], kind="bar", figsize=(16, 9))
+
 # print bar graph
 mp.show()
