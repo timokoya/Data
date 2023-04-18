a84f938912804beaf1baa82d66e4438259a9fb3a
Timi Okoya <125748895+timokoya@users.noreply.github.com>
2023-04-18 14:55:21 +0100

    Update DataPlay.py
    - increased columns in dataframe
    
    - remove comma and currency symbo in columns
    
    - cast value in column to float
    
    - print bar graphs

--- DefensePoint/DataPlay.py
+++ DefensePoint/DataPlay.py
@@ -6,10 +6,16 @@ import matplotlib.pyplot as mp
 data = pd.read_csv("data.csv", nrows=25)
 
 # select 2 columns
-df = pd.DataFrame(data, columns=["discounted_price", "rating"])
+df = pd.DataFrame(data, columns=["discounted_price", "rating", "discount_percentage", "actual_price"])
 
 # remove the currency symbol in the discounted_price column
 df['discounted_price'] = df['discounted_price'].str.replace('₹', '')
+
+# remove the currency symbol in the actual_price column
+df['actual_price'] = df['actual_price'].str.replace('₹', '')
+
+# remove the comma in the actual_price column
+df['actual_price'] = df['actual_price'].str.replace(',', '')
 
 # remove the comma in the discounted_price column
 df['discounted_price'] = df['discounted_price'].str.replace(',', '')
@@ -26,8 +32,23 @@ df['discounted_price']=df['discounted_price'].astype(float)
 # cast the rating value into float
 df['rating']=df['rating'].astype(float)
 
-# plot the dataframe
+# cast the rating value into float
+df['actual_price']=df['actual_price'].astype(float)
+
+# plot rating vs discounted_price 
 df.plot(x="discounted_price", y=["rating"], kind="bar", figsize=(16, 9))
 
-# print bar graph
+# print bar graph of rating vs discounted_price
+mp.show()
+
+# plot rating vs discount_percentage 
+df.plot(x="discount_percentage", y=["rating"], kind="bar", figsize=(16, 9))
+
+# print bar graph of rating vs discount_percentage
+mp.show()
+
+# plot actual_price vs discount_percentage 
+df.plot(x="discount_percentage", y=["actual_price"], kind="bar", figsize=(16, 9))
+
+# print bar graph of actual_price vs discount_percentage 
 mp.show()
